c3528a0513408c5fe9e3042b0119431089384056
Yosmel Chiang <yosmelchiang@gmail.com>
2025-10-11 07:22:06 +0200

    notes on aria labeling

---
+++ 0.navbars/vertical-navbar/react-vertical-navbar-v1/src/components/nav.jsx
@@ -0,0 +1,14 @@
+function Nav() {
+    return (
+              <nav>
+                <ul>
+                  <li><a href="/about"><i class="fa-regular fa-user" aria-hidden="true"></i> About</a></li>
+                  <li><a href="/hobbies"><i class="fa-regular fa-headphones" aria-hidden="true"></i> Hobbies</a></li>
+                  <li><a href="/contact"><i class="fa-regular fa-envelope" aria-hidden="true"></i> Contact</a></li>
+                  <li><a href="#" aria-label="Twitter"><i class="fa-brands fa-twitter" aria-hidden="true" ></i></a></li>
+                </ul>
+              </nav>
+    )
+}
+
+export default Nav
