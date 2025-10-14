91f3b9ac99cb26310dbe0bcea09bef4933161d94
Yosmel Chiang <yosmelchiang@gmail.com>
2025-10-14 13:28:20 +0200

    add basic functinality to the navbar buttons

---
+++ responsive-navbar/r-navbar/src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+export default function () {
+  return (
+    <header>
+    <a href="#navbar"><i class="fa-solid fa-bars" aria-hidden="true"></i></a>
+    <nav id="navbar">
+      <ul>
+        <li><a href="#"><i class="fa-solid fa-xmark" aria-hidden="true"></i></a></li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+        <li>
+          <a href="#">Blog</a>
+        </li>
+      </ul>
+    </nav>
+    </header>
+  );
+}
