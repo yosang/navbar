411cfe3f949bca507a45b8a05efa2451cb33b13e
Yosmel Chiang <yosmelchiang@gmail.com>
2025-10-14 14:49:59 +0200

    add margin to the header

--- responsive-navbar/r-navbar/src/App.jsx
+++ responsive-navbar/r-navbar/src/App.jsx
@@ -5,8 +5,8 @@ export default function () {
     <header>
     <a id="hamburger-btn" href="#navbar"><i class="fa-solid fa-bars" aria-hidden="true"></i></a>
     <nav id="navbar">
+      <a id="close-btn" href="#"><i class="fa-solid fa-xmark" aria-hidden="true"></i></a>
       <ul>
-        <li><a href="#"><i class="fa-solid fa-xmark" aria-hidden="true"></i></a></li>
         <li>
           <a href="#">About</a>
         </li>
